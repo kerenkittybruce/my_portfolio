1524ad53368430e47036e1cee5d8a73e68fd81e6
Keren Bruce <kerenchelseabruce@gmail.com>
2022-11-05 13:37:53 +0200

    master

---
+++ action-page.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
+
+Thank you for sharing your thoughts with me ☺️💕.
+I will get back to you as soon as possible.
+
+Have a lovely day, and please be safe out there.
+</body>
+</html>
